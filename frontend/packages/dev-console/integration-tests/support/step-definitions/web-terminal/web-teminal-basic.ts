fe51e28a9e17e60887074fc2759b53b89a7e76bf
musienko maksym <mmusiien@redhat.com>
2021-06-21 22:15:01 +0300

    Apply proposals to PR

--- frontend/packages/dev-console/integration-tests/support/step-definitions/web-terminal/web-teminal-basic.ts
+++ frontend/packages/dev-console/integration-tests/support/step-definitions/web-terminal/web-teminal-basic.ts
@@ -4,7 +4,7 @@ import { webTerminalPage } from '../../pages/web-terminal/webTerminal-page';
 When('user clicks on Open Terminal in new tab button on the terminal window', () => {
   webTerminalPage.verifyOpenInNewTabButton();
 });
-And('user close current Web Terminal session', () => {
+Then('user close current Web Terminal session', () => {
   webTerminalPage.closeCurrentTerminalSession();
 });
 Then('user will see the terminal window opened in new tab', () => {
@@ -13,12 +13,12 @@ Then('user will see the terminal window opened in new tab', () => {
 
 And('user does nothing with displayed terminal window 1 minutes', () => {
   // override the defauld idling timeout form 15 minute to 1 minute
-  cy.exec (`oc patch configmap devworkspace-controller -n openshift-operators --patch "
+  cy.exec(`oc patch configmap devworkspace-controller -n openshift-operators --patch "
   data:
     devworkspace.idle_timeout: '1m'
-  "`)
+  "`);
   const terminalIdlingTimeout: number = Number(Cypress.env('TERMINAL_IDLING_TIMEOUT')) || 60000;
-  cy.wait(terminalIdlingTimeout)
+  cy.wait(terminalIdlingTimeout);
   webTerminalPage.verifyInnactivityMessage(terminalIdlingTimeout);
 });
 
